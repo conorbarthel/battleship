7fc54783d4de481e54517fd0787947a5bd94e353
Ian Ross <ianrossdrums@gmail.com>
2021-12-09 12:27:19 -0700

    feat: add render method to cell class

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,6 +1,7 @@
+require 'pry'
 require './lib/ship'
 require './lib/cell'
-Rspec.describe "cell" do
+RSpec.describe "cell" do
   #Tests to see if the cell instance exists
   it "initalizes" do
     cell = Cell.new("B4")
@@ -12,14 +13,15 @@ end
 
   it "is empty?" do
     cell = Cell.new("B4")
-    expect(cell.empty?).to (true)
+    expect(cell.empty?).to eq(true)
   end
 
   it "places ship" do
     cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
     #checks to see if the ship was placed, nothing is returned
-    expect(cell.ship).to eq(Ship)
+    expect(cell.ship).to be_a(Ship)
   end
 
   it "fired upon?" do
@@ -30,11 +32,86 @@ end
 
   it "fire upon" do
     cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
     cell.fire_upon
-    expects(cell.fire_upon?).to eq(true)
-    expects(cell.ship)
+
+    expect(cell.ship.health).to eq(2)
+    expect(cell.fired_upon?).to eq(true)
   end
 
+  describe '#render' do
+    it "prints '.'" do
+      cell_1 = Cell.new("B4")
+#prints . by default
+      expect(cell_1.render).to eq(".")
+    end
 
+    it "prints 'M'" do
+      cell_1 = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_1.fire_upon
+#prints M if fired_upon == true and empty? == true
+      expect(cell_1.render).to eq("M")
+    end
 
+    it "prints '.'" do
+      cell_1 = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_2 = Cell.new("C3")
+#prints . if empty? == false and render has no argument
+      expect(cell_1.render).to eq(".")
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+      cell_2.place_ship(cruiser)
+      expect(cell_2.render).to eq(".")
+    end
+
+    it "prints 'S'" do
+      cell_1 = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_2 = Cell.new("C3")
+#prints S if empty == false and render has an argument of true
+      expect(cell_1.render).to eq(".")
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+      cell_2.place_ship(cruiser)
+      expect(cell_2.render).to eq(".")
+      expect(cell_2.render(true)).to eq("S")
+    end
+
+    it "prints 'H'" do
+      cell_1 = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_2 = Cell.new("C3")
+#prints H if empty? returns false and fired_upon? returns true
+      expect(cell_1.render).to eq(".")
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+      cell_2.place_ship(cruiser)
+      expect(cell_2.render).to eq(".")
+      expect(cell_2.render(true)).to eq("S")
+      cell_2.fire_upon
+      expect(cell_2.render).to eq("H")
+      cruiser.hit
+    end
+
+    it "prints 'X'" do
+      cell_1 = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_2 = Cell.new("C3")
+#combination of all tests. New test is prints X if sunk?==true
+      expect(cell_1.render).to eq(".")
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+      cell_2.place_ship(cruiser)
+      expect(cell_2.render).to eq(".")
+      expect(cell_2.render(true)).to eq("S")
+      cell_2.fire_upon
+      expect(cell_2.render).to eq("H")
+      cruiser.hit
+      cruiser.hit
+      expect(cell_2.render).to eq("X")
+    end
+  end
 end
