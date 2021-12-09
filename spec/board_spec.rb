6e1e51ea1dac75efdbe3ad13ce364e66bfb8b5f5
Conor Barthel <conor.barthel@gmail.com>
2021-12-09 08:30:35 -0700

    Test: add test for valid_cordinate?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,13 +1,25 @@
 require './lib/board'
 RSpec.describe "Board" do
-  it "is a Board" do
-    board = Board.new
+  # it "is a Board" do
+  #   board = Board.new
+  #
+  #   expect(board).to be_a(Board)
+  # end
+  #
+  # it "returns a hash of 16 Cells" do
+  #   board = Board.new
+  #
+  #   expect(board.cells).to be_a(Hash)
+  #   expect(board.cells.size).to eq(16)
+  # end
 
-    expect(board).to be_a(Board)
-  end
+  decribe '#valid_cordinate?' do
+    it "returns true or false if valid" do
+      board = Board.new
 
-  it "returns a hash of 16 Cells" do
-    board = Board.new
+      expect(board.valid_coordinate?("A1")).to be(true)
+      expect(board.valid_coordinate?("A5")).to be(false)
+    end
   end
 
 end
