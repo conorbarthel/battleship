32ec507f0131123c95a91c7847b4157f0d787720
Ian Ross <ianrossdrums@gmail.com>
2021-12-09 19:27:13 -0700

    feat: add valid coordinate method to board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,6 @@
-require './lib/board'
+require './lib/board.rb'
+require './lib/cell.rb'
+require 'pry'
 RSpec.describe "Board" do
   describe "is a valid board" do
 
@@ -22,13 +24,13 @@ RSpec.describe "Board" do
       expect(board.cells.values.sample).to be_a(Cell)
     end
 
-    xit "validates_cordinates" do
+    it "validates_cordinates" do
       board = Board.new
 
       expect(board.valid_coordinate?("A1")).to be(true)
       expect(board.valid_coordinate?("D4")).to be(true)
-      expect(board.valid_coordinate?("A5")).to be(true)
-      expect(board.valid_coordinate?("E1")).to be(true)
+      expect(board.valid_coordinate?("A5")).to be(false)
+      expect(board.valid_coordinate?("E1")).to be(false)
       expect(board.valid_coordinate?("A22")).to be(false)
     end
   end
