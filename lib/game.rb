4debe729c9ad419680a0caa9477fab822423b877
Conor Barthel <conor.barthel@gmail.com>
2021-12-12 11:46:04 -0700

    Feat: methods for welcome and user_input

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+class Game
+
+  def welcome
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+  end
+
+  def user_input
+    input = gets.chomp
+  end
+
+
+end
+
+# game = Game.new
+#
+# game.welcome
