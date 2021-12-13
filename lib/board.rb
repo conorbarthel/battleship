0fe3400c41f9788c95d5dd29d54ba2babd5bddda
Ian Ross <ianrossdrums@gmail.com>
2021-12-12 17:00:22 -0700

    refactor: change rendering board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,35 +1,33 @@
-require 'pry'
+require './lib/cell.rb'
+require './lib/ship.rb'
 class Board
   attr_reader  :cells
 
-  def initialize(cells)
+  def initialize
     @cells = {
-     "A1" = Cell.new("A1"),
-     "A2" = Cell.new("A2"),
-     "A3" = Cell.new("A3"),
-     "A4" = Cell.new("A4"),
-     "B1" = Cell.new("B1"),
-     "B2" = Cell.new("B2"),
-     "B3" = Cell.new("B3"),
-     "B4" = Cell.new("B4"),
-     "C1" = Cell.new("C1"),
-     "C2" = Cell.new("C2"),
-     "C3" = Cell.new("C3"),
-     "C4" = Cell.new("C4"),
-     "D1" = Cell.new("D1"),
-     "D2" = Cell.new("D2"),
-     "D3" = Cell.new("D3"),
-     "D4" = Cell.new("D4")
+     "A1" => Cell.new("A1"),
+     "A2" => Cell.new("A2"),
+     "A3" => Cell.new("A3"),
+     "A4" => Cell.new("A4"),
+     "B1" => Cell.new("B1"),
+     "B2" => Cell.new("B2"),
+     "B3" => Cell.new("B3"),
+     "B4" => Cell.new("B4"),
+     "C1" => Cell.new("C1"),
+     "C2" => Cell.new("C2"),
+     "C3" => Cell.new("C3"),
+     "C4" => Cell.new("C4"),
+     "D1" => Cell.new("D1"),
+     "D2" => Cell.new("D2"),
+     "D3" => Cell.new("D3"),
+     "D4" => Cell.new("D4")
     }
   end
 
-  def get_keys
-    @cells.keys
+  def valid_coordinate?(cord)
+    @cells.keys.any?(cord)
   end
 
-  def valid_coordinate?(cord)
-    get_keys.any?(cord)
-  end
 
   def place(ship, coords)
     coords.each do |coord|
@@ -59,30 +57,19 @@ class Board
   end
 
   def num_consec?(cords)
-    last(cords).each_cons(2) do |two_things|
-     return false if two_things[0].next != two_things[1]-1
+    last(cords).each_cons(2) do |pair|
+     return false if pair[0].next != pair[1]
     end
     true
   end
 
   def alpha_consec?(cords)
-    first(cords).each_cons(2) do |two_things|
-     return false if two_things[0].next != two_things[1]
+    first(cords).each_cons(2) do |pair|
+     return false if pair[0].next != pair[1]
     end
     true
   end
 
-<<<<<<< HEAD
-  def valid_placement?(ship, cordinates)
-    cords = split_cords(cordinates)
-    if ship.length != cordinates.size
-      false
-    elsif !alpha_consec?(cords) || !num_consec?(cords)
-      false
-    elsif alpha_consec?(cords) && last(cords).all?(last(cords)[0])
-      true
-    elsif num_consec?(cords) && first(cords).all?(first(cords)[0])
-=======
   def overlapping_ship?(coordinates)
     coordinates.any? do |coord|
       if !(@cells[coord].empty?)
@@ -110,23 +97,20 @@ class Board
     if num_consec?(coordinates) && first(coordinates).all?(first(coordinates)[0])
       true
     elsif alpha_consec?(coordinates) && last(coordinates).all?(last(coordinates)[0])
->>>>>>> 8987e7339575693ca569dbcffc0cf925912b2796
       true
+    else
+      false
     end
   end
-<<<<<<< HEAD
 
-  def render(display = false)
-    @display = display
-    if display = true
-      board_rend = "  1 2 3 4
-      \nA #{@cells["A1"]} #{@cells["A2"]} #{@cells["A3"]} #{@cells["A4"]}
-      \nB #{@cells["B1"]} #{@cells["B2"]} #{@cells["B3"]} #{@cells["B4"]}
-      \nC #{@cells["C1"]} #{@cells["C2"]} #{@cells["C3"]} #{@cells["C4"]}
-      \nD #{@cells["D1"]} #{@cells["D2"]} #{@cells["D3"]} #{@cells["D4"]}"
-    end
-    board_rend
+  def render_b#(display = false)
+    #@display = display
+    #if display == true
+      puts row1 = "  1 2 3 4\n"
+      puts row2 = "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}\n"
+      puts row3 = "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}\n"
+      puts row4 = "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}\n"
+      puts row5 = "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}"
+    #end
   end
-=======
->>>>>>> 8987e7339575693ca569dbcffc0cf925912b2796
 end
