9790d1b665bacf9703cc97519d96dc9d0977e764
Ian Ross <ianrossdrums@gmail.com>
2021-12-13 10:29:17 -0700

    feat: add board exists test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe 'game' do
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-    @ships = [cruiser, submarine]
-    @game = Game.new(board, ships)
+    @ships = [@cruiser, @submarine]
+    @game = Game.new(@board, @ships)
   end
 
   it 'exists' do
     expect(@game).to be_a(Game)
   end
 
-
+  it "has a board" do
+    expect(@board).to be_a(Board)
+  end
 end
