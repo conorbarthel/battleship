172327ac4f11fafe01b3506e54d3e7242f92a2f5
Ian Ross <ianrossdrums@gmail.com>
2021-12-15 09:31:50 -0700

    feat: add welcome message test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,25 +25,30 @@ RSpec.describe 'game' do
       expect(@game.cpu_valid_coords).to eq(["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"])
     end
 
+    describe "welcome message" do
+      it "prints welcome message" do
+        expect(@game.welcome_message)
+      end
+    end
     # xit "prints welcome message" do
     #   expect(@game.welcome_message).to eq(puts "Welcome to BATTLESHIP \n" "Enter p to play. Enter q to quit.")
     # end
   end
 
-  describe "cpu functionality" do
-    it "cpu cords are correct length" do
-      expect(@game.get_cpu_coords(@cpu_cruiser).length).to eq(3)
-    end
-    it "cpu_cords are valid" do
-      expect(@game.cpu_board.valid_placement?(@cpu_cruiser, @game.get_cpu_coords(@cpu_cruiser))).to eq(true)
-    end
-    xit "places ships" do
-      @game.cpu_placement(@game.cpu_board.ship_1)
-      binding.pry
-
-      expect(@game.cpu_board.cells.values).to eq(@cpu_cruiser)
-    end
-  end
+  # describe "cpu functionality" do
+  #   it "cpu cords are correct length" do
+  #     expect(@game.get_cpu_coords(@cpu_cruiser).length).to eq(3)
+  #   end
+  #   it "cpu_cords are valid" do
+  #     expect(@game.cpu_board.valid_placement?(@cpu_cruiser, @game.get_cpu_coords(@cpu_cruiser))).to eq(true)
+  #   end
+  #   xit "places ships" do
+  #     @game.cpu_placement(@game.cpu_board.ship_1)
+  #     binding.pry
+  #
+  #     expect(@game.cpu_board.cells.values).to eq(@cpu_cruiser)
+  #   end
+  # end
 
 
 end
