99addd2ecda1323bc5ef6a39e830122efcf20b42
Ian Ross <ianrossdrums@gmail.com>
2021-12-15 14:52:38 -0700

    refactor: ship comments

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,17 +1,17 @@
 class Ship
   attr_reader :name, :length, :health
-#Initializes name and length parameters
+# Initializes name and length parameters
   def initialize(name, length)
     @name = name
     @length = length
     @health = length #health is assigned to the length of the ship
   end
 
-  # When the health of a ship is equal to zero return true, that it did sink
+# When the health of a ship is equal to zero return true, that it did sink
   def sunk?
     @health == 0
   end
-  # Subtract one health from the ship that got hit
+# Subtract one health from the ship that got hit
   def hit
     @health -= 1
   end
