d1c8ceb2bd4b33ec9b1f6cb2e3383395992d86ca
Ian Ross <ianrossdrums@gmail.com>
2021-12-14 17:26:26 -0700

    feat: add set up and turn methods

--- lib/game.rb
+++ lib/game.rb
@@ -14,16 +14,15 @@ class Game
   end
 
   def welcome_message
-    puts "Welcome to BATTLESHIP" +
+    puts "Welcome to BATTLESHIP! " +
     "Enter p to play. Enter q to quit."
     play
   end
 
-
   def play
     input = gets.chomp
     if input == "p"
-      turn
+      set_up(player_board, cpu_board)
     elsif input == "q"
       welcome_message
     elsif input == "stop"
@@ -34,24 +33,25 @@ class Game
     welcome_message
   end
 
-  def set_up
-    display_boards(player_board, cpu_board)
+  def set_up(player_board, cpu_board)
+    display_board(player_board, cpu_board)
     cpu_placement(cpu_board.ship_1)
     cpu_placement(cpu_board.ship_2)
     place_instructions
     player_placement(player_board.ship_1)
     player_placement(player_board.ship_2)
+    turn(player_board, cpu_board)
   end
 
 
   def turn(player_board, cpu_board)
-    if player_board.ship_1.sunk? && player_board.ship_2.sunk?
-      puts "I won!"
-      welcome_message
-    elsif cpu_board.ship_1.sunk? && cpu_board.ship_2.sunk?
-      puts "You won!"
+    player_shot
+    cpu_shot
+    if player_board.ship_1.sunk? && player_board.ship_2.sunk? || cpu_board.ship_1.sunk? && cpu_board.ship_2.sunk?
+      puts "Game over"
       welcome_message
     end
+    turn(player_board, cpu_board)
   end
 
   def display_board(p_board, c_board)
@@ -65,14 +65,21 @@ class Game
     valid_choices = @player_valid_coords
     puts "Enter a coordinate to shoot at: \n"
     shot = gets.chomp
-    return puts "Invalid coordinates. Please try again: \n" if @player_valid_coords.any?(shot) == false
-    if player_board.cells[shot].empty?
-      puts "Your shot #{shot} is a miss"
-    elsif player_board.cells[shot].empty? == false
-      puts "Your shot #{shot} is a Hit!"
-    elsif player_board.cells[shot].empty? == false && player_board.cells[shot].ship.sunk?
-      puts "Your shot #{shot} is a Hit! You sunk my BATTLESHIP!"
+    cpu_board.cells[shot].fire_upon
+
+    if @player_valid_coords.any?(shot) == false
+       puts "Invalid coordinates. Please try again: \n"
+       player_shot
+    elsif cpu_board.cells[shot].empty?
+      puts "Your shot #{shot} is a miss \n"
+    elsif cpu_board.cells[shot].empty? == false
+      puts "Your shot #{shot} is a Hit! \n"
+    elsif cpu_board.cells[shot].empty? == false && cpu_board.cells[shot].ship.sunk?
+      puts "Your shot #{shot} is a Hit! You sunk my BATTLESHIP! \n"
     end
+  
+    puts "COMPUTER BOARD"
+    cpu_board.render_b(true)
   end
 
 # gets valid coords for CPU
@@ -92,24 +99,28 @@ class Game
       cpu_board.place(ship, get_cpu_coords(ship))
   end
 
-  def computer_shot
+  def cpu_shot
     shot = @cpu_valid_coords.delete(@cpu_valid_coords.sample)
-    cpu_board.cells[shot].fire_upon
-    cpu_board.render_b #might want to take this linie out should the cpu be rendering?
-    if cpu_board.cells[shot].empty?
-      puts "My shot #{shot} is a miss"
-    elsif cpu_board.cells[shot].empty? == false
-      puts "My shot #{shot} is a Hit!"
-    elsif cpu_board.cells[shot].empty? == false && cpu_board.cells[shot].ship.sunk?
-      puts "My shot #{shot} is a Hit! I sunk your BATTLESHIP!"
+    player_board.cells[shot].fire_upon
+    if player_board.cells[shot].empty?
+      puts "My shot #{shot} is a miss \n" +
+      ""
+    elsif player_board.cells[shot].empty? == false
+      puts "My shot #{shot} is a Hit! \n" +
+      ""
+    elsif player_board.cells[shot].empty? == false && player_board.cells[shot].ship.sunk?
+      puts "My shot #{shot} is a Hit! I sunk your BATTLESHIP! \n" +
+      ""
     end
+    puts "PLAYER BOARD"
+    player_board.render_b(true)
   end
 
   def place_instructions
     puts "The computer has placed their ships"
     puts "Now it is your turn to lay out your ships"
     puts "The cruiser is 3 units long and the submarine is 2 units"
-    @board.render_b
+    @player_board.render_b
   end
 
   def player_placement(ship)
@@ -144,4 +155,4 @@ cpu_board = Board.new(cruiser, submarine)
 player_board = Board.new(cruiser, submarine)
 game = Game.new(cpu_board, player_board)
 
-p game.player_placement(cruiser)
+game.welcome_message
